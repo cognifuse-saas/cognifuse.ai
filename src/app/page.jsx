0534c11e0dbfa90ee11d583e33b8ae6a9119e35c
Dennis Murage <dmurage@gmail.com>
2024-09-06 19:47:12 +0300

    Bug fix, unrelevant data removal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -182,7 +182,7 @@ export default function Web() {
               <a href="#">Technological edge <span aria-hidden="true">&rarr;</span></a>
               <a href="#">Meet our team <span aria-hidden="true">&rarr;</span></a>
             </div>
-            <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+            {/* <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
               <div className="flex flex-col-reverse">
                 <dt className="text-base leading-7 text-gray-300">Years in operation</dt>
                 <dd className="text-2xl font-bold leading-9 tracking-tight text-white">5+</dd>
@@ -199,7 +199,7 @@ export default function Web() {
                 <dt className="text-base leading-7 text-gray-300">African languages supported</dt>
                 <dd className="text-2xl font-bold leading-9 tracking-tight text-white">20+</dd>
               </div>
-            </dl>
+            </dl> */}
           </div>
         </div>
       </div>
